536ff0edd95979a517cc89b1f13c7797a9ba8355
TA�ARA FERNANDA SANCHES DIAS <tayara.5970@aluno.pr.senac.br>
2023-09-18 16:03:23 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/tayara/Tayara.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tayara;
+
+/**
+ *
+ * @author tayara.5970
+ */
+public class Tayara {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
